18783c7bea129e608ee7c4f63768fd8f17d46bf5
naim <naiim.chatti@gmail.com>
2016-05-18 14:07:14 +0200

    vide

--- www/js/Controller.js
+++ www/js/Controller.js
@@ -7,54 +7,55 @@ angular.module('starter.controllers', [])
 
     $scope.categorie = $stateParams.categorie;
 
-    console.log(data);
-    $scope.programme = {
-      jour1: {
-          0 : {
-            chaine : "tf1",
-            titre : "bbb",
-            heure : "20H45",
-            durée : "bb",
-            categorie : "science-fiction"
-          },
-          1 :{
-            chaine : "tf1",
-            titre : "bb",
-            heure : "20H45",
-            durée : "bb",
-            categorie : "bb"
-          },
-          2 :{
-            chaine : "tf1",
-            titre : "bb",
-            heure : "20H45",
-            durée : "bb",
-            categorie : "sf"
-          },
-          3 :{
-            chaine : "tf1",
-            titre : "bb",
-            heure : "20H45",
-            durée : "bbb",
-            categorie : "bbb"
-          },
-          4 :{
-            chaine : "france 2",
-            titre : "bbb",
-            heure : "20H45",
-            durée : "bbb",
-            categorie : "bb"
-          },
-          5 :{
-            chaine : "tf1",
-            titre : "bbb",
-            heure : "20H45",
-            durée : "bb",
-            categorie : "bbb"
-          }
-        }
-    }
-    console.log($scope.programme);
+    $scope.programme = [
+      jour1 = [
+          0 =[
+            chaine = "tf1",
+            titre = "bbb",
+            heure = "20H45",
+            durée = "bb",
+            categorie = "science-fiction"
+          ],
+          1 = [
+            chaine = "tf1",
+            titre = "bb",
+            heure = "20H45",
+            durée = "bb",
+            categorie = "bb"
+          ],
+          2 =[
+            chaine = "tf1",
+            titre = "bb",
+            heure = "20H45",
+            durée = "bb",
+            categorie = "sf"
+          ],
+          3 = [
+            chaine = "tf1",
+            titre = "bb",
+            heure = "20H45",
+            durée = "bbb",
+            categorie = "action"
+          ],
+          4 = [
+            chaine = "france 2",
+            titre = "bbb",
+            heure = "20H45",
+            durée = "bbb",
+            categorie = "science-fiction"
+          ],
+          5 = [
+            chaine = "tf1",
+            titre = "bbb",
+            heure = "20H45",
+            durée = "bb",
+            categorie = "action"
+          ]
+        ]
+    ];
+     console.log($scope.programme);
+
+
 
   }
 });

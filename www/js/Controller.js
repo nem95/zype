20669489d666dda5cb3d86799db14d6027f3d84a
naim <naiim.chatti@gmail.com>
2016-05-21 00:24:52 +0200

    Selection multiple fonctionnel
    ATTENTION ne pas clicker sur policier en premier sinon ca vide l'array et ca bug

--- www/js/Controller.js
+++ www/js/Controller.js
@@ -1,59 +1,76 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngStorage',])
 
-    .controller('ListsConttroller', function ($scope, $stateParams, $ionicModal, $http, $state) {
+    .controller('ListsConttroller', function ($scope, $stateParams, $ionicModal, $http, $state, $localStorage) {
 
+      $scope.$storage = $localStorage.$default({
+          liste:'',
+          currentCategorie:'',
+        });
 
-        $scope.categorie = [];
-        $scope.programs = [];
-
-        $scope.addCategory = function(data){
+        $scope.categorie = ["film-policier"];
+        $scope.program = [];
+        /*$scope.addCategory = function(data){
           $scope.categorie.push(data);
           console.log($scope.categorie);
+        }*/
+        $scope.addCategory = function(data){
 
-          /*angular.forEach($scope.categorie, function(value, key) {
 
-            var i = key + 1;
+          console.log(data);
+
+          angular.forEach($scope.categorie, function(value, key) {
+
             console.log(data);
+            console.log(key);
             console.log(value);
 
             if (data == value) {
               console.log('hello');
-              $scope.categorie.splice(key );
+              $scope.categorie.splice(key, 1);
+            }else {
+              $scope.categorie.push(data);
+
             }
 
-            i++;
-            console.log(i);
+            console.log($scope.categorie);
 
-          });*/
-
-
-          //return  $scope.categorie;
+          });
 
         }
 
         $scope.getCategory = function(data){
           console.log(data);
-          $scope.listCategory = data;
-          angular.forEach($scope.listCategory, function(value, key){
-            console.log(value);
-            //value = "fiction" = []
-            $http.post("http://timothee-dorand.fr/zype/www/script/getprogram.php?categorie=" + value).success(function (data) {
-              $scope.program = data;
-              $scope.programs.push($scope.program);
+          if (data != 0) {
+            $state.transitionTo("lists");
+          }
+          //on reset le localstorage a chaque nouvel selection
+          $localStorage.$reset();
 
-              console.log($scope.programs);
+          $scope.listCategory = data; // $scope.listCategory = array []
+
+          //on boucle sur le tableau $scope.listCategory
+          angular.forEach($scope.listCategory, function(value, key){
+
+            //console.log(value);
+            // on fait la requete au serveur pour chaque valeur du tableau
+            $http.post("http://timothee-dorand.fr/zype/www/script/getprogram.php?categorie=" + value).then(function (res) {
+              var data = res.data;
+              //on ajouter les resultats de la requete au tableau program déclarer tout en haut
+              $scope.program.push(data);
+
+              // on dit que $scope.programs est egale au tableau program
+              $scope.programs = $scope.program;
+
+
+              console.log($scope.programs[0]);
+              // on ajoute le tout au localstorage
+              $scope.$storage.currentCategorie = $scope.programs;
+              console.log($scope.$storage.currentCategorie);
+
             });
           });
-
-          $state.transitionTo("lists");
-
         }
-
-
-
-
-
-
+        console.log($scope.$storage.currentCategorie);
 
     })
 

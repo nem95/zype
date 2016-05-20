32a61d7163e28a79fc2b6ce6b2032c5f0cc666cb
timothee <tim.dorand@gmail.com>
2016-05-20 08:58:13 +0200

    db removed from git

---
+++ platforms/android/assets/www/js/Controller.js
@@ -0,0 +1,62 @@
+angular.module('starter.controllers', [])
+
+    .controller('ListsConttroller', function ($scope, $stateParams, $ionicModal, $http) {
+        getProgram($stateParams.categorie);
+        function getProgram() {
+            $http.post("http://timothee-dorand.fr/zype/www/script/getprogram.php?categorie=" + $stateParams.categorie).success(function (data) {
+                $scope.programs = data;
+            });
+        };
+
+    })
+
+    .controller('InfoConttroller', function ($scope, $stateParams, $ionicModal, $http) {
+
+        getInfo($stateParams.title);
+        function getInfo() {
+            $http.post("http://timothee-dorand.fr/zype/www/script/getinfo.php?title=" + $stateParams.title).success(function (data) {
+                $scope.info = data;
+            });
+        };
+
+    });
+
+/*
+    .controller('SimpleController',function ($scope,$http){
+
+        $scope.chooseProgramForm = function (add) {
+            var data= {
+                comedie:$scope.newChoice.comedie,
+                policier:$scope.newChoice.policier,
+                drame:$scope.newChoice.drame
+            }
+            $http.post("http://localhost:8888/zype/www/script/chooseProgramForm.php",
+                {
+                    'comedie': $scope.newChoice.comedie,
+                    'policier': $scope.newChoice.policier,
+                    'drame': $scope.newChoice.drame
+                })
+                .success(function (data, status, headers, config) {
+                    console.log("inserted Successfully");
+                });
+            $scope.choixprogramme.push(data);
+            $scope.newChoice = {
+                comedie:"",
+                policier:"",
+                drame:""
+            };
+        };
+
+    })*/
+
+/*
+
+    .controller('myController', ['$scope', 'PhpService',
+        function ($scope, PhpService) {
+            $scope.usuario = {};
+            $scope.enviar = function () {
+                var enviar = {};
+                enviar = $.param({"enviado": JSON.stringify($scope.usuario)}); //convertimos a url string todos los parametros para enviarlos como tipo 'form'
+                PhpService.enviar(enviar);
+            };
+        }]);*/
--- platforms/ios/www/script/db.php
+++ platforms/ios/www/script/db.php
@@ -3,14 +3,18 @@
 define('SQL_USERNAME',   'root');
 define('SQL_PASSWORD',   'root');
 define('SQL_DBNAME',	 'zype_tv');*/
+
 define('SQL_HOST',       'timotheefxtim.mysql.db');
 define('SQL_USERNAME',   'timotheefxtim');
 define('SQL_PASSWORD',   'Adminparis96');
 define('SQL_DBNAME',	 'timotheefxtim');
+
 try {
     $db = new PDO('mysql:dbname='.SQL_DBNAME.';host='.SQL_HOST, SQL_USERNAME, SQL_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 }
+
 catch(Exception $e) {
     exit('Erreur : ' . $e->getMessage());
 }
+
 ?>
